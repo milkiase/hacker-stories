2616b7e33cdd361cd86d2fa1554dcaf5c57de4e0
milkiase <milktes27@gmail.com>
2023-06-17 16:46:57 -0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  var helloWorld = "Welcome to the road to hacker stories by Milkias!"
+  return (
+    <>
+      <h2> {helloWorld} </h2>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
