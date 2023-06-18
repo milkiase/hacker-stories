3dc79995e6c030b91bd3ab70d42acfe409fee0e5
milkiase <milktes27@gmail.com>
2023-06-18 16:41:13 -0200

    Rendering List

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import * as React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-  var helloWorld = "Welcome to the road to hacker stories by Milkias!"
+  const blockElements = [
+      {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+      },
+      {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  
+  let favBooks = ['Rich dad poor dad', 'Get Strong', 'The Male Advantage', 'The Power of Habits', 'Can\'t Hurt Me', 'So Good They Cannot Ignore You']
   return (
-    <>
-      <h2> {helloWorld} </h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="">
+        <ul>
+          {
+            blockElements.map((element)=>{
+              return (
+                <li key={element.objectID}>
+                  <span>
+                    <a className=" text-blue-700" href={element.url} target="_blank" rel="noopener noreferrer">{element.title}</a>
+                  </span>
+                  <span>{element.author} </span>
+                  <span> {element.num_comments} </span>
+                  <span> {element.points} </span>
+                </li>
+              );
+            })
+          }
+        </ul>
+        <ul className=" list-disc">
+          {
+            favBooks.map((book, index)=>{
+              return (<li className="list-disc" key={index}>{book}</li>)
+            })
+          }
+        </ul>
+        <label htmlFor="message">Message: </label>
+        <input type="text" id="message" />
       </div>
-    </>
   )
 }
 
