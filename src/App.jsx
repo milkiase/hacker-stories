598555026f802dd7c28436b5b1f6549e366aac45
milkiase <milktes27@gmail.com>
2023-06-21 11:12:56 -0200

    props distructuring implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,67 @@
-// import * as React from 'react'
+import * as React from 'react'
 
-function App() {
-  const blockElements = [
-      {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-      },
-      {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
+
+const App = ()=>{
+  const stories = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
     },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
   ];
+  const [searchTerm, searchTermHandler] = React.useState('react')
+  const searchedStories = stories.filter((story)=>story.title.toLowerCase().includes(searchTerm.toLowerCase()))
   
-  let favBooks = ['Rich dad poor dad', 'Get Strong', 'The Male Advantage', 'The Power of Habits', 'Can\'t Hurt Me', 'So Good They Cannot Ignore You']
-  return (
-      <div className="">
-        <ul>
-          {
-            blockElements.map((element)=>{
-              return (
-                <li key={element.objectID}>
-                  <span>
-                    <a className=" text-blue-700" href={element.url} target="_blank" rel="noopener noreferrer">{element.title}</a>
-                  </span>
-                  <span>{element.author} </span>
-                  <span> {element.num_comments} </span>
-                  <span> {element.points} </span>
-                </li>
-              );
-            })
-          }
-        </ul>
-        <ul className=" list-disc">
-          {
-            favBooks.map((book, index)=>{
-              return (<li className="list-disc" key={index}>{book}</li>)
-            })
-          }
-        </ul>
-        <label htmlFor="message">Message: </label>
-        <input type="text" id="message" />
+  const handleSearch = (event)=>{
+    searchTermHandler(event.target.value)
+  }
+  const resetSearchTerm = ()=>searchTermHandler('react')
+  return <div className="">
+        <Search onSearch={handleSearch} searchTerm={searchTerm} className=' mt-12' /> 
+        <button onClick={resetSearchTerm} className=' bg-teal-600 rounded-sm ml-3 px-2 py-1'>Reset</button>
+        <List list={searchedStories}/>
       </div>
-  )
 }
 
+const List = ({list})=>
+    <ul>
+      {
+        list.map(({objectID, ...element})=>  <Item key={objectID} {...element}></Item>)
+      }
+    </ul>
+
+const Item = ({url, title, author, num_comments, points})=>
+    <div>
+      <li className=" ">
+        <span>
+          <a className=" text-blue-700" href={url} target="_blank" rel="noopener noreferrer">{title}</a>
+        </span>
+        <br />Authors: <span>{author} </span>
+        <br />Comments: <span> {num_comments} </span>
+        <br />Points: <span> {points} </span>
+      </li>
+      <hr />
+    </div>
+
+const Search = ({searchTerm, onSearch})=>{
+  return (
+    <>
+      <label htmlFor="message">Search: </label>
+      <input onChange={onSearch} type="text" value={searchTerm} id="message" className=" px-2 py-1 border rounded-sm border-gray-950"/>
+    </>
+  );
+}
+    
+  
 export default App
