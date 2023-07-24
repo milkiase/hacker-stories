feb5adf47b410f08460f78bbfca15b77d9c8e9b4
milkiase <milktes27@gmail.com>
2023-07-24 05:08:09 +0300

    sorting img path changed

--- src/components/Switch/index.jsx
+++ src/components/Switch/index.jsx
@@ -3,22 +3,15 @@ import './style.css'
 const Switch = ({onSwitchToggle, switchOn})=>{
     return (
         <>
-        {// <div className="ml-3 mt-1 toggle-button-cover">
-        // <div id="button-3" className="button r">
-        //     <input className="checkbox" type="checkbox" onChange={onSwitchToggle} checked={switchOn}/>
-        //     <div className="knobs"></div>
-        //     <div className="layer"></div>
-        // </div>
-        }
         <input type="checkbox" id="checkboxInput" onChange={onSwitchToggle} checked={switchOn}/>
     <label htmlFor="checkboxInput" className="toggleSwitch ml-2">
 
 <div className="speaker w-5">
-    <img src="src\components\Switch\assets\sort-ascending.png" alt="asc" />
+    <img src="https://img.icons8.com/?size=512&id=kK48r4Y0UNpR&format=png" alt="asc" />
 </div>
 
 <div className="mute-speaker w-5">    
-    <img src="src\components\Switch\assets\sort-descending.png" alt="desc" />
+    <img src="https://img.icons8.com/?size=512&id=31487&format=png" alt="desc" />
 </div>
 
     </label>
